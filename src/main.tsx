8e8e45ca420256e1475249eea1beeac1b974314f
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-02-25 11:27:26 +0300

    added loader

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./index.css"
 import Experience from "./Experience"
+import { Loader } from "@react-three/drei"
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<Experience />
+		<Loader />
 	</StrictMode>
 )
